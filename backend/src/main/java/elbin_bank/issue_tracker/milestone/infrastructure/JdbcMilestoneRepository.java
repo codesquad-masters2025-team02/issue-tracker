7a93b4d28c7f49371ed68a1738947a3f0ced0a04
guzzang <991012kim@gmail.com>
2025-05-16 23:51:01 +0900

    fix(JdbcMilestoneRepository): change findMilestonesByMilestoneIds method

--- backend/src/main/java/elbin_bank/issue_tracker/milestone/infrastructure/JdbcMilestoneRepository.java
+++ backend/src/main/java/elbin_bank/issue_tracker/milestone/infrastructure/JdbcMilestoneRepository.java
@@ -1,15 +1,13 @@
 package elbin_bank.issue_tracker.milestone.infrastructure;
 
-import elbin_bank.issue_tracker.milestone.domain.Milestone;
 import elbin_bank.issue_tracker.milestone.domain.MilestoneRepository;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
-import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcMilestoneRepository implements MilestoneRepository {
@@ -21,59 +19,28 @@ public class JdbcMilestoneRepository implements MilestoneRepository {
     }
 
     @Override
-    public List<Milestone> findMilestonesByMilestoneIds(List<Long> milestoneIds) {
+    public Map<Long,String> findMilestonesByMilestoneIds(List<Long> milestoneIds) {
         if(milestoneIds == null || milestoneIds.isEmpty()) {
-            return List.of();
+            return Map.of();
         }
 
         String sql = """
-        SELECT *
+        SELECT id, title
         FROM milestone
         WHERE id IN (:milestoneIds)
         """;
 
         var params = new MapSqlParameterSource("milestoneIds", milestoneIds);
 
-        return jdbc.query(sql, params, milestoneRowMapper());
+        return jdbc.query(sql, params, rs -> {
+            Map<Long, String> result = new HashMap<>();
+            while (rs.next()) {
+                result.put(rs.getLong("id"), rs.getString("title"));
+            }
+            return result;
+        });
 
     }
-
-    private RowMapper<Milestone> milestoneRowMapper() {
-        return (rs, rowNum) -> {
-            Milestone milestone = new Milestone();
-
-            // Milestone 고유 필드
-            milestone.setId(rs.getLong("id"));
-            milestone.setTitle(rs.getString("title"));
-            milestone.setDescription(rs.getString("description"));
-            milestone.setClosed(rs.getBoolean("is_closed"));
-
-            Date expiredDate = rs.getDate("expired_at");
-            if (expiredDate != null) {
-                milestone.setExpiredAt(expiredDate.toLocalDate());
-            }
-
-            //Not null
-            Timestamp createdAt = rs.getTimestamp("created_at");
-            milestone.setCreatedAt(createdAt.toLocalDateTime());
-
-
-            // BaseEntity 공통 필드
-            Timestamp updatedAt = rs.getTimestamp("updated_at");
-            if (updatedAt != null) {
-                milestone.setUpdatedAt(updatedAt.toLocalDateTime());
-            }
-
-            Timestamp deletedAt = rs.getTimestamp("deleted_at");
-            if (deletedAt != null) {
-                milestone.setDeletedAt(deletedAt.toLocalDateTime());
-            }
-
-            return milestone;
-        };
-    }
-
-
 
 }
 
