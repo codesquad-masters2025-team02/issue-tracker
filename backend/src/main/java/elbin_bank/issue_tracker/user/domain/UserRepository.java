5d62603967786b8096f4e3d34a20fd9e316edfe5
guzzang <991012kim@gmail.com>
2025-05-21 17:05:15 +0900

    style(UserRepository): delete unused package

--- backend/src/main/java/elbin_bank/issue_tracker/user/domain/UserRepository.java
+++ backend/src/main/java/elbin_bank/issue_tracker/user/domain/UserRepository.java
@@ -1,10 +1,16 @@
 package elbin_bank.issue_tracker.user.domain;
 
+import elbin_bank.issue_tracker.comment.application.query.dto.UserDto;
+
 import java.util.List;
 import java.util.Map;
 
 public interface UserRepository {
 
     Map<Long, String> findNickNamesByIds(List<Long> ids);
+
+
+
+
 
 }
