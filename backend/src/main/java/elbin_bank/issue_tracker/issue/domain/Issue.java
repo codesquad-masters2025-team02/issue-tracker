6eb7cdaa71f4cb46f9e1ee77dbaae25d46b88842
HeeSung Choi <gladhee@icloud.com>
2025-05-24 12:35:28 +0900

    refactor(Issue): remove unused import for Column annotation

--- backend/src/main/java/elbin_bank/issue_tracker/issue/domain/Issue.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/domain/Issue.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("issue")
