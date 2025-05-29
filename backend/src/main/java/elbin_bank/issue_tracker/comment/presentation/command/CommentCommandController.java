19a14f06f4a6a7dff07f159a4f3f8f443b25e719
guzzang <991012kim@gmail.com>
2025-05-29 12:21:59 +0900

    refactor: delete unused package

--- backend/src/main/java/elbin_bank/issue_tracker/comment/presentation/command/CommentCommandController.java
+++ backend/src/main/java/elbin_bank/issue_tracker/comment/presentation/command/CommentCommandController.java
@@ -1,6 +1,5 @@
 package elbin_bank.issue_tracker.comment.presentation.command;
 
-import elbin_bank.issue_tracker.comment.application.query.CommentsQueryRepository;
 import elbin_bank.issue_tracker.comment.domain.CommentCommandRepository;
 import elbin_bank.issue_tracker.comment.presentation.command.dto.request.CommentCreateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +14,8 @@ public class CommentCommandController {
 
     private final CommentCommandRepository commentCommandRepository;
 
-    @PostMapping(s)
-    public ResponseEntity<Void> createComment(@RequestBody CommentCreateRequestDto commentCreateRequestDto, @PathVariable Long id, String s){
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<Void> createComment(@RequestBody CommentCreateRequestDto commentCreateRequestDto, @PathVariable Long id){
         commentCommandRepository.save(commentCreateRequestDto, id);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
