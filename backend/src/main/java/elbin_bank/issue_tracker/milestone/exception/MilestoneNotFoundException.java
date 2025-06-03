feb0d19defe53b3bfda68ce50098f148ae269695
guzzang <991012kim@gmail.com>
2025-06-03 14:31:13 +0900

    fix(MilestoneNotFoundException): fix error message

--- backend/src/main/java/elbin_bank/issue_tracker/milestone/exception/MilestoneNotFoundException.java
+++ backend/src/main/java/elbin_bank/issue_tracker/milestone/exception/MilestoneNotFoundException.java
@@ -2,6 +2,6 @@ package elbin_bank.issue_tracker.milestone.exception;
 
 public class MilestoneNotFoundException extends RuntimeException {
     public MilestoneNotFoundException(Long id) {
-        super("Could not find label with id " + id);
+        super("Could not find milestone with id " + id);
     }
 }
