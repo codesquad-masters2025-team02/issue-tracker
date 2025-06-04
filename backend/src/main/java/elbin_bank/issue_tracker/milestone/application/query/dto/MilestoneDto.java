803b4f47d3ac8a8c20fa1c59f1b3b3a1bb047f39
HeeSung Choi <gladhee@icloud.com>
2025-06-04 22:20:10 +0900

    feat(MilestoneDto): add description field to MilestoneDto

--- backend/src/main/java/elbin_bank/issue_tracker/milestone/application/query/dto/MilestoneDto.java
+++ backend/src/main/java/elbin_bank/issue_tracker/milestone/application/query/dto/MilestoneDto.java
@@ -3,6 +3,7 @@ package elbin_bank.issue_tracker.milestone.application.query.dto;
 public record MilestoneDto(
         long id,
         String title,
+        String description,
         String expiredAt,
         int progressRate,
         long totalIssueCount,
